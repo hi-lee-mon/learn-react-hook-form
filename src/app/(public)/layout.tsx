5d81e92bc4e6449e805d568f7abb63596e4b4a72
hi-lee-mon <shunmel.09@gmail.com>
2025-09-27 14:21:43 +0900

    feat: レイアウトのヘッダーの背景色とリンクボタンのスタイルを更新
    ヘッダーの背景色を変更し、リンクボタンのフォントウェイトを強調しました。

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,14 +2,23 @@ import LinkButton from "@/components/LinkButton";
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-      <header className="sticky top-0 w-full z-50  flex justify-between mb-4 bg-gray-500/20 backdrop-blur-lg  shadow-lg rounded-br-lg rounded-bl-lg">
-        <LinkButton href="/" variant="text" size="large" color="inherit">
+      <header className="sticky top-0 w-full z-50  flex justify-between mb-4 bg-gray-800/20 backdrop-blur-lg  shadow-lg rounded-br-lg rounded-bl-lg">
+        <LinkButton
+          href="/"
+          variant="text"
+          size="large"
+          color="inherit"
+          sx={{ fontWeight: "bold" }}
+        >
           Learn React Hook Form
         </LinkButton>
         <nav>
           <ul>
             <li>
-              <LinkButton href="/base" sx={{ textDecoration: "underline" }}>
+              <LinkButton
+                href="/base"
+                sx={{ textDecoration: "underline", fontWeight: "bold" }}
+              >
                 基礎
               </LinkButton>
             </li>
