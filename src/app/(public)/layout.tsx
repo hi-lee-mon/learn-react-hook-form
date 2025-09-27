4b4c230dbf280650e33ad1fa751db181c5951fe4
hi-lee-mon <shunmel.09@gmail.com>
2025-09-27 14:11:01 +0900

    feat: レイアウトのヘッダーのスタイルを更新
    ヘッダーの背景色とシャドウを追加し、デザインを改善しました。

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,7 +2,7 @@ import LinkButton from "@/components/LinkButton";
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-      <header className="sticky top-0 border-b w-full z-50  flex justify-between mb-4 bg-black opacit">
+      <header className="sticky top-0 w-full z-50  flex justify-between mb-4 bg-gray-500/20 backdrop-blur-lg  shadow-lg rounded-br-lg rounded-bl-lg">
         <LinkButton href="/" variant="text" size="large" color="inherit">
           Learn React Hook Form
         </LinkButton>
